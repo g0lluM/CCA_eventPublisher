66976aba97c062ebd2fe5a4e6ab95aec5e7fd5c9
Antonin <antonintessier@gmail.com>
2016-07-01 11:40:34 +0200

    Date and subject ok

--- check_fields.js
+++ check_fields.js
@@ -2,11 +2,12 @@ function check()
 {
     var $place = document.getElementById("eventPlace").value;
     var $date = document.getElementById("eventDate").value;
-    var $dateReg = /%d%d\//;
-    
+        
     if ($place == null || $place == "" || $date == null || $date == "")
     {
 	alert("You have to specify a date and a place. The abstract is not compulsory.");
 	return false;
     }
+
+    return true;
 }
